d805f96f2a5b99df37a8e4c014854aa31c70092b
Joshua DeVille <40128827+devillejoshua14@users.noreply.github.com>
2022-04-21 23:23:55 -0400

    Add files via upload

---
+++ android_university_lab6-master/app/src/main/java/com/codepath/nytimes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codepath.nytimes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
